7029a1979253ced51d86a6ffd2d7189bf99c3a61
jamcoy <james@coynemail.net>
2016-12-21 12:00:37 +0000

    Add authentication by JSON web tokens

--- django_todo_project/urls.py
+++ django_todo_project/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
 from todo.views import TodoView
+from accounts.views import RegistrationView
  
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^todo/$', TodoView.as_view()),
-    url(r'^todo/(?P<pk>[0-9]+)/$', TodoView.as_view())
+    url(r'^todo/(?P<pk>[0-9]+)/$', TodoView.as_view()),
+    url(r'^accounts/register/$', RegistrationView.as_view()),
+    url(r'^accounts/api-token-auth/$', obtain_jwt_token)
 ]
